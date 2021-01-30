b73ecb7c1c4b4d38e1525be2569afd55accae5a1
Caroline Lynch <carolineablynch@gmail.com>
2021-01-30 17:17:09 -0500

    writing functions in JS file to retrieve and save notes

---
+++ assets/index.js
@@ -0,0 +1,25 @@
+const $noteTitle = $(".note-title");
+const $noteBody = $(".note-body");
+const $saveBtn = $(".saveBtn");
+const $newBtn = $(".newBtn");
+const $noteList = $(".list-container .list-group");
+
+let currentNote = {};
+
+//function to retrieve notes 
+const getNotes = () => {
+    return $.ajax({
+        url: "/api/notes",
+        method: "GET",
+    });
+};
+
+//function to save notes 
+const saveNote = (note) => {
+    return $.ajax ({
+        url: "/api/notes",
+        data: note,
+        method: "POST",
+    });
+};
+
