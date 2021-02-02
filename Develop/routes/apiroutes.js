5ab2c01b53a03762a4159c90a38353e4c82b0beb
Caroline Lynch <carolineablynch@gmail.com>
2021-02-01 19:38:27 -0500

    saving notes with unique ID

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -2,6 +2,7 @@
 
 const fs = require("fs");
 const db = require("../db/db.json");
+const ID = require("ID"); 
 
 module.exports = function (app) {
 
@@ -16,13 +17,42 @@ app.get("/api/notes", function(req, res) {
 }); 
 
 //new note POST 
+
 app.post("/api/notes", function (req, res) {
     fs.readFile("db/db.json", function(err, data) {
         if (err) throw err;
         let notes = JSON.parse(data);
-        const newNote {
+        const newNote = {
             title: req.body.title,
             text: req.body.text,
+            id: ID.generate()
         };
 
-    }
+    console.log(db)
+    notes.push(newNote);
+    fs.writeFile("db/db.json", JSON.stringify(notes, null, 2), (err) => {
+        if(err) throw err;
+        res.send("200");
+        })
+    });
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+    fs.readFile("db/db.json", function(err, data) {
+        const deleteNote = req.params.id;
+        if(err) throw err;
+        let notes = JSON.parse(data);
+        for (let i=0; i < notes.length; i++) {
+            if(notes[i].id === deleteNote){
+                notes.splice(i, 1);
+            };
+        };
+    fs.writeFile("db/db.json", JSON.stringify(notes, null, 2), (err) => {
+        if(err) throw err;
+        res.send('200');
+        });
+    });
+    });
+};
+
+
