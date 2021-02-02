10c099dad10912f05af2756bb7ad72adb73b4727
Caroline Lynch <carolineablynch@gmail.com>
2021-02-01 19:21:32 -0500

    adding POST to create new note

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -1,4 +1,28 @@
 //Connecting route to notes 
 
 const fs = require("fs");
-const db = ("../db/db.json");
+const db = require("../db/db.json");
+
+module.exports = function (app) {
+
+// GET request 
+
+app.get("/api/notes", function(req, res) {
+    fs.readFile("db/db.json", function(err,data) {
+        if(err) throw err; 
+        let notes = JSON.parse(data);
+        return res.json(notes);
+    });
+}); 
+
+//new note POST 
+app.post("/api/notes", function (req, res) {
+    fs.readFile("db/db.json", function(err, data) {
+        if (err) throw err;
+        let notes = JSON.parse(data);
+        const newNote {
+            title: req.body.title,
+            text: req.body.text,
+        };
+
+    }
